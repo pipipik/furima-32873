b280a4cf203173e729ad8053a6f818bce2cbff05
pipipik <hiro.konan913@gmail.com>
2021-03-25 15:50:36 +0900

    ユーザー管理機能修正1

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, presence: true
-  validates :birthday, presence: true
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
-    validates :first_name
-    validates :last_name
-  end
-  with_options presence: true, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/ } do
-    validates :first_name_kana
-    validates :last_name_kana
+  
+  validates_format_of :password, with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  with_options presence: true do
+    validates :nickname, :birthday
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+      validates :first_name
+      validates :last_name
+    end
+    with_options format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/ } do
+      validates :first_name_kana
+      validates :last_name_kana
+    end
   end
 end
+  
