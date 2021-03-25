89390eb06d94a0f35c694f8992999bb4d3e9365b
pipipik <hiro.konan913@gmail.com>
2021-03-26 00:17:02 +0900

    Active Storageの導入

--- Gemfile
+++ Gemfile
@@ -62,4 +62,5 @@ end
 
 gem 'devise'
 gem 'pry-rails'
-
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,5 +16,8 @@ class User < ApplicationRecord
       validates :last_name_kana
     end
   end
+
+  has_many :items
+  
 end
   
