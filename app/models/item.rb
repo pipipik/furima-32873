7e25804c4adbde7ef8a1504eb06e2a73f5a71f64
pipipik <hiro.konan913@gmail.com>
2021-03-29 22:34:34 +0900

    バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,15 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :scheduled_delivery
 
+  with_options presence: true do
+    validates :image, :name, :info
+    with_options numericality: { other_than: 1 } do
+      validates :category_id, :status_id, :shipping_fee_id, :prefecture_id, :scheduled_delivery_id
+    end
+    with_options numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 } do
+      validates :price
+    end
+  end
+
+
 end
