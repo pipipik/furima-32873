fdb70aebeab5c01fe81d0f1b1621adabb6fd304d
pipipik <hiro.konan913@gmail.com>
2021-04-21 18:59:39 +0900

    フォームの保存

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,28 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
 
   def index
     @order_address = OrderAddress.new
   end
-  
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:order_address).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id).merge(item_id: @item.id)
+  end
+
 end
